7764a7ec27ce1ece3ce00ecb8f9c50d5ab27414d
harshrajput5099 <184426429+harshrajput5099@users.noreply.github.com>
2026-04-21 23:40:59 +0530

    Update Dependencies

--- frontend/ai_office_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/ai_office_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
+import flutter_secure_storage_macos
 import open_file_mac
 import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
